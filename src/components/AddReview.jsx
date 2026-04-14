b030487eaf228bf98e88c648078152fb188c7178
fathy soliman <fathi123soliman@gmail.com>
2026-04-14 12:24:20 +0300

    s

---
+++ src/components/AddReview.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react"
+import axios from "axios"
+
+const AddReview = ({ addReview, setAddReview }) => {
+  const init = {
+    commit: "",
+    rating: "",
+  }
+
+  const [formState, setFormState] = useState(init)
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+
+    const response = await axios.post("http://localhost:3001/review", formState)
+
+    let List = { ...addReview }
+    List.push(response.data)
+    setAddReview(List)
+
+    setFormState(init)
+  }
+
+  const handleChange = (event) => {
+    setFormState({ ...formState, [event.target.name]: event.target.value })
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="commit">comment</label>
+      <textarea
+        name="commit"
+        cols="30"
+        rows="10"
+        onChange={handleChange}
+        value={formState.commit}
+      ></textarea>
+      <label htmlFor="rating">Rate:</label>
+      <select name="rating" onChange={handleChange} value={formState.rating}>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+      </select>
+      <button type="submit">Add</button>
+    </form>
+  )
+}
+
+export default AddReview
