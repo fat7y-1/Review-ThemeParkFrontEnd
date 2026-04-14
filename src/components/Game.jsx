516c2773e9206469e20f79426008d2a2bba96aee
YUSUF-ALSHIEKH <yousifsheikh19@gmail.com>
2026-04-14 14:39:27 +0300

    nothing

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,25 +1,36 @@
+import { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
-import { useNavigate } from "react-router-dom"
-const Game = ({ games }) => {
-  let navigate = useNavigate()
-  const deleteButton=async(event , id)=>{
-    event.preventDefault()
-    let response = await axios.get(`http://localhost:3001/game/${id}`)
-    navigate("/")
+import AddReview from "./AddReview"
+import axios from "axios"
 
-  }
-  const { id } = useParams()
-  let selectedGame
-  games.forEach((game) => {
-    if (game._id === id) {
-      selectedGame = game
+const Game = ({ games }) => {
+  const [addReview, setAddReview] = useState([])
+
+  useEffect(() => {
+    const getReview = async () => {
+      try {
+        let response = await axios.get("http://localhost:3001/review")
+        setAddReview(response.data)
+      } catch (err) {
+        console.log(err)
+      }
     }
-  })
+
+    getReview()
+  }, [])
+
+  const { id } = useParams()
+  const selectedGame = games.find((game) => game._id === id)
+
+  if (!selectedGame) return <div>Loading game details...</div>
+
   return (
     <div>
       <h1>{selectedGame.name}</h1>
       <img src={selectedGame.image} alt="game image" />
       <p>{selectedGame.desc}</p>
+
+      <AddReview addReview={addReview} setAddReview={setAddReview} id={id} />
     </div>
   )
 }
